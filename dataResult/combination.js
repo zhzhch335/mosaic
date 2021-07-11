d57eafa3a6629677574ba134b8e7e561d8f9b8e7
zhzhch335 <357611628@qq.com>
2021-07-11 21:19:19 +0800

    图片url与祝福绑定

--- dataResult/combination.js
+++ dataResult/combination.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs")
 const path = require("path")
+const _ = require("lodash")
 
 
 // 获取图片内容
@@ -16,12 +17,19 @@ blessData.forEach((item, index) => {
     // 查找图片作者和祝福作者同名的数据
     let indexOfUser = imgData.findIndex(img=>img.author === item.dduser)
     if (indexOfUser !== -1) {
-        // 如果找到了就拼接图像和祝福的信息
-        result.push(Object.assign(item,imgData[indexOfUser]))
-        // 并在图片集合中删除数据
-        imgData.splice(indexOfUser,1)
-        // 不能立即删除已经输入的祝福，否则会影响遍历，因此设定一个标记
-        item.used = true
+        // 保存对应的url数据
+        let url = imgData[indexOfUser].url
+        // 循环替换所有url符合的imgData
+        while(indexOfUser !== -1) {
+            // 如果找到了就拼接图像和祝福的信息
+            result.push(Object.assign(JSON.parse(JSON.stringify(item)),JSON.parse(JSON.stringify(imgData[indexOfUser]))))
+            // 并在图片集合中删除数据
+            imgData.splice(indexOfUser,1)
+            // 不能立即删除已经输入的祝福，否则会影响遍历，因此设定一个标记
+            item.used = true
+            // 查找下一个url
+            indexOfUser = imgData.findIndex(img=>img.url === url)
+        }
     }
 });
 
@@ -33,7 +41,7 @@ blessData.filter(item=>!item.used).forEach(item => {
 })
 
 if (imgData.length) {
-    console.log(`剩余图片${imgData.length}个`)
+    console.log(`剩余图片${_.uniqBy(imgData,'url').length}个`)
     imgData.forEach(item=>{
         result.push(item)
     })
@@ -43,6 +51,6 @@ if (blessData.filter(item=>!item.used).length) {
     console.log(`剩余祝福${blessData.filter(item=>!item.used).length}个`)
 }
 
-fs.writeFile(path.join(__dirname, '/result.json'), JSON.stringify(result), err => {
+fs.writeFile(path.join(__dirname, '/database.json'), JSON.stringify(result), err => {
     if(err) console.log(err)
 })
